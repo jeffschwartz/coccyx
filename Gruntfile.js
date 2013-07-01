b19b0c90f83f204aa9dc16c14df4896641560f9d
jeffschwartz <jefftschwartz@gmail.com>
2013-07-01 19:19:04 -0400

    Added grunt. Refactored demo app into its own directory.

---
+++ Gruntfile.js
@@ -0,0 +1,57 @@
+module.exports = function ( grunt ) {
+
+    // Project configuration.
+    grunt.initConfig( {
+        pkg    : grunt.file.readJSON( 'package.json' ),
+        concat : {
+            options : {
+                separator : ';'
+            },
+            dist    : {
+                src  : [
+                    'javascripts/libs/jquery.js',
+                    'javascripts/frontendcontroller.js',
+                    'javascripts/helpers.js',
+                    'javascripts/history.js',
+                    'javascripts/model.js',
+                    'javascripts/router.js',
+                    'javascripts/view.js'
+                    ],
+                dest : 'javascripts/dist/coccyx.js'
+            },
+            demoapp : {
+                src  : [
+                    'javascripts/libs/jquery.js',
+                    'javascripts/frontendcontroller.js',
+                    'javascripts/helpers.js',
+                    'javascripts/history.js',
+                    'javascripts/model.js',
+                    'javascripts/router.js',
+                    'javascripts/view.js'
+                    ],
+                dest : 'demoapp/javascripts/coccyx.js'
+            }
+        },
+        watch  : {
+            scripts : {
+                files   : '**/*.js',
+                tasks   : ['concat'],
+                options : {
+                    interrupt : true
+                }
+            }
+        }
+    } );
+
+    // Load the plugins
+    grunt.loadNpmTasks( 'grunt-contrib-watch' );
+    grunt.loadNpmTasks( 'grunt-contrib-concat' );
+
+    // Default task(s).
+    grunt.registerTask( 'default', ['concat'] );
+
+    // Alias Tasks
+    // grunt.registerTask( 'dev', 'Running Grunt dev', ['concat:dist'] );
+    // grunt.registerTask( 'prod', 'Running Grunt prod', ['concat:dist'] );
+
+};
