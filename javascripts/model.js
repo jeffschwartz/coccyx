c5b87eb7964dc6c54788e73c5a712d2094319a2d
jeffschwartz <jefftschwartz@gmail.com>
2013-07-01 17:41:53 -0400

    Added model.js to project.

---
+++ javascripts/model.js
@@ -0,0 +1,15 @@
+(function($){
+    "use strict";
+
+    var Coccyx = window.Coccyx = window.Coccyx || {};
+
+
+    function modelFactory(metaModel){
+
+    }
+
+    Coccyx.models = {
+        factory: modelFactory
+    };
+
+}(jQuery));
