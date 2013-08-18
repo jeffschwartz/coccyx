cfa3d11a56c6e4610e2a67a77eba767fcd2ae3fe
jeffschwartz <jefftschwartz@gmail.com>
2013-08-18 15:09:07 -0400

    Implemented dom events into views.

--- javascripts/dist/coccyx.js
+++ javascripts/dist/coccyx.js
@@ -962,7 +962,22 @@
 ;define('views', ['jquery'], function($){
     'use strict';
 
-    var Coccyx = window.Coccyx = window.Coccyx || {};
+    var Coccyx = window.Coccyx = window.Coccyx || {},
+        domEventTopic = 'DOM_EVENT';
+
+    //0.6.0
+    function domEventDispatcher(event){
+        Coccyx.pubsub.publish(domEventTopic, {viewName: event.data.viewName, event: event});
+    }
+
+    //0.6.0
+    //domEventsHash = {viewName: 'viewname', events['event selector',...]}.
+    function wireDomEvents(domEventsHash){
+        domEventsHash.events.forEach(function(event){
+            var a = event.split(' ');
+            Coccyx.$(a[1]).on(a[0], {viewName: domEventsHash.viewName}, domEventDispatcher);
+        });
+    }
 
     //0.5.0
     function extend(viewObject){
@@ -972,11 +987,16 @@
         obj.$ = $;
         //0.6.0
         obj.$domTarget = viewObject.hasOwnProperty('domTarget') ? $(viewObject.domTarget) : undefined;
+        //0.6.0
+        if(viewObject.hasOwnProperty('domEvents')){
+            wireDomEvents(viewObject.domEvents);
+        }
         return obj;
     }
 
     Coccyx.views = {
-        extend: extend
+        extend: extend,
+        domEventTopic: domEventTopic
     };
 
 });
--- javascripts/views.js
+++ javascripts/views.js
@@ -1,7 +1,22 @@
 define('views', ['jquery'], function($){
     'use strict';
 
-    var Coccyx = window.Coccyx = window.Coccyx || {};
+    var Coccyx = window.Coccyx = window.Coccyx || {},
+        domEventTopic = 'DOM_EVENT';
+
+    //0.6.0
+    function domEventDispatcher(event){
+        Coccyx.pubsub.publish(domEventTopic, {viewName: event.data.viewName, event: event});
+    }
+
+    //0.6.0
+    //domEventsHash = {viewName: 'viewname', events['event selector',...]}.
+    function wireDomEvents(domEventsHash){
+        domEventsHash.events.forEach(function(event){
+            var a = event.split(' ');
+            Coccyx.$(a[1]).on(a[0], {viewName: domEventsHash.viewName}, domEventDispatcher);
+        });
+    }
 
     //0.5.0
     function extend(viewObject){
@@ -11,11 +26,16 @@ define('views', ['jquery'], function($){
         obj.$ = $;
         //0.6.0
         obj.$domTarget = viewObject.hasOwnProperty('domTarget') ? $(viewObject.domTarget) : undefined;
+        //0.6.0
+        if(viewObject.hasOwnProperty('domEvents')){
+            wireDomEvents(viewObject.domEvents);
+        }
         return obj;
     }
 
     Coccyx.views = {
-        extend: extend
+        extend: extend,
+        domEventTopic: domEventTopic
     };
 
 });
