31f7cb74d3adf487f5b6cb88cd1e3423b2ecc941
jeffschwartz <jefftschwartz@gmail.com>
2013-08-19 17:38:30 -0400

    Rewrote how views deal with dom events. No longer using pubsub, and now using traditional jQuery event callbacks.

--- javascripts/views.js
+++ javascripts/views.js
@@ -2,39 +2,55 @@ define('views', ['jquery'], function($){
     'use strict';
 
     var Coccyx = window.Coccyx = window.Coccyx || {},
-        domEventTopic = 'DOM_EVENT';
+        domEventTopic = 'DOM_EVENT',
+        proto;
 
     //0.6.0
-    function domEventDispatcher(event){
-        Coccyx.pubsub.publish(domEventTopic, {viewName: event.data.viewName, event: event});
-    }
-
-    //0.6.0
-    //domEventsHash = {viewName: 'viewname', events['event selector',...]}.
-    function wireDomEvents(viewObject){
-        viewObject.domEvents.events.forEach(function(eventDescription){
-            var a = eventDescription.split(' ');
-            Coccyx.$(viewObject.domTarget).on(a[0], a[1], {viewName: viewObject.domEvents.viewName, eventType: a[0], eventSelector: a[1]}, domEventDispatcher);
-        });
-    }
-
-    //0.5.0
-    function extend(viewObject){
-        // Create a new object using the view object as its prototype.
-        var obj =  Object.create(viewObject);
-        // Decorate the new object with additional properties.
-        obj.$ = $;
-        //0.6.0
-        obj.$domTarget = viewObject.hasOwnProperty('domTarget') ? $(viewObject.domTarget) : undefined;
-        //0.6.0
-        if(viewObject.hasOwnProperty('domEvents')){
-            wireDomEvents(viewObject);
+    //Wire view dom events to callback methods in the controller
+    //using the context of the controller when calling the callbacks.
+    //domEventsHash = {controller: controller, events: {'event selector': callback, ...}}.
+    function wireDomEvents(domEventsHash, $domTarget, namespace){
+        var prop;
+        var a;
+        for(prop in domEventsHash.events){
+            if(domEventsHash.events.hasOwnProperty(prop)){
+                a = prop.split(' ');
+                $domTarget.on(a[0] + namespace, a[1],
+                    domEventsHash.events[prop].bind(domEventsHash.controller));
+            }
         }
-        return obj;
     }
+
+    //0.6.0
+    function remove(viewObject){
+        viewObject.$domTarget.off(viewObject.namespace);
+    }
+
+    //0.5.0, 0.6.0
+    function extend(viewObject, domEventsHash){
+        // Create a new object using the view object as its prototype.
+        var obj1 =  Coccyx.helpers.extend(Object.create(proto), viewObject);
+        var obj2 = Object.create(obj1);
+        obj2.$domTarget = viewObject.hasOwnProperty('domTarget') ? $(viewObject.domTarget) : undefined;
+        if(domEventsHash){
+            obj2.namespace = '.' + Date.now().toString();
+            wireDomEvents(typeof domEventsHash === 'function' ? domEventsHash() : domEventsHash, obj2.$domTarget, obj2.namespace);
+        }
+        return obj2;
+    }
+
+    //0.6.0
+    proto = {
+        remove: function(){
+            this.$domTarget.off(this.namespace);
+            this.$domTarget.empty();
+        },
+        $: $
+    };
 
     Coccyx.views = {
         extend: extend,
+        remove: remove,
         domEventTopic: domEventTopic
     };
 
