32c1db6305a941bdee74112b65a1f9126605491b
jeffschwartz <jefftschwartz@gmail.com>
2013-08-30 10:04:06 -0400

    Added default settings to ajax methods. Added merging of user's setting with default setting in ajax methods.

--- javascripts/dist/coccyx.js
+++ javascripts/dist/coccyx.js
@@ -1176,30 +1176,36 @@ define('eventer', ['jquery'], function($){
 ;define('ajax', ['jquery'], function($){
     'use strict';
 
-    var Coccyx = window.Coccyx = window.Coccyx || {};
+    var Coccyx = window.Coccyx = window.Coccyx || {},
+        helpers = Coccyx.helpers,
+        defaultSettings = {cache: false, url: '/'};
 
-    //A promise-based wrapper around jQuery Ajax.
-    //All methods return a Promise.
+        //Mege default setting with user's settings.
+        function mergeSettings(settings){
+            return helpers.extend({}, defaultSettings, settings);
+        }
+
+    //A simple promise-based wrapper around jQuery Ajax. All methods return a Promise.
     Coccyx.ajax = {
-        //http GET
+        //http "GET"
         ajaxGet: function(settings){
             settings.type = 'GET';
-            return $.ajax(settings);
+            return $.ajax(mergeSettings(settings));
         },
-        //http POST
+        //http "POST"
         ajaxPost: function(settings){
             settings.type = 'POST';
-            return $.ajax(settings);
+            return $.ajax(mergeSettings(settings));
         },
-        //http PUT
+        //http "PUT"
         ajaxPut: function(settings){
             settings.type = 'PUT';
-            return $.ajax(settings);
+            return $.ajax(mergeSettings(settings));
         },
-        //http DELETE
+        //http "DELETE"
         ajaxDelete: function(settings){
             settings.type = 'DELETE';
-            return $.ajax(settings);
+            return $.ajax(mergeSettings(settings));
         }
     };
 
