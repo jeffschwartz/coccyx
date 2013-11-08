7a8fdea2394018eda090a6d2377ac26ab2e0b716
jeffschwartz <jefftschwartz@gmail.com>
2013-11-08 12:38:40 -0500

    Cleaned up comments and code in views.

--- javascripts/views.js
+++ javascripts/views.js
@@ -6,32 +6,27 @@ define('views', ['application', 'helpers'], function(){
         proto;
 
     //0.6.0
-    //Wire view dom events to callback methods in the controller
-    //using the context of the controller when calling the callbacks.
-    //domEventsHash = {controller: controller, events: {'event selector': callback, ...}}.
+    //Wire view dom events to callback methods in the controller using the context of the controller when
+    //calling the callbacks. domEventsHash = {controller: controller, events: {'event selector': callback, ...}}.
     function wireDomEvents(domEventsHash, $domTarget, namespace){
         var prop, a;
         for(prop in domEventsHash.events){
             if(domEventsHash.events.hasOwnProperty(prop)){
                 a = prop.split(' ');
-                $domTarget.on(a[0] + namespace, a[1],
-                    domEventsHash.events[prop].bind(domEventsHash.controller));
+                $domTarget.on(a[0] + namespace, a[1], domEventsHash.events[prop].bind(domEventsHash.controller));
             }
         }
     }
 
     //0.6.0
-    //Every view must have a domTarget property, which is used to render the view.
-    //Views can be rendered in 1 of 2 ways, either "detached" or "attached" to the DOM.
-    //To render as detached, provide either a tagName property ('div', 'section', 'article',
-    //'span', etc.) and a domTargetAttrs property (a hash, that can contain an id property, a class
-    //property and other element attributes as properties), or set the domTarget property (either
-    //a string, whose value would be appropriate for calling document.createElement(domTarget),
-    //or a callback to a function that returns a string appropriate for calling
-    //document.createElement(domTarget)) yourself, or omit domTarget and domTargetAttrs altogether
-    //and a default domTarget will be provided for you, which will be a plain "<div>" element.
-    //To render as attached, set $domTarget to a valid jquery object and domTarget will be created
-    //for you from $domTarget.
+    //Every view must have a domTarget property, which is used to render the view. Views can be rendered in 1 of 2 ways, either
+    //"detached" or "attached" to the DOM. To render as detached, provide either a tagName property ('div', 'section', 'article',
+    //'span', etc.) and a domTargetAttrs property (a hash, that can contain an id property, a class property and other element
+    //attributes as properties), or set the domTarget property (either a string, whose value would be appropriate for calling
+    //document.createElement(domTarget), or a callback to a function that returns a string appropriate for calling
+    //document.createElement(domTarget)) yourself, or omit domTarget and domTargetAttrs altogether and a default domTarget will be
+    //provided for you, which will be a plain "<div>" element. To render as attached, set $domTarget to a valid jquery object and
+    //domTarget will be created for you from $domTarget.
     function setTarget(){
         /*jshint validthis:true*/
         if(this.$domTarget && this.$domTarget instanceof v.$){
@@ -43,8 +38,7 @@ define('views', ['application', 'helpers'], function(){
             this.$domTarget = v.$(this.domTarget);
         }else if(this.domTargetAttrs){
             //Use domTargetAttrs.
-            this.$domTarget = v.$(document.createElement(this.tagName ?
-               this.tagName : 'div')).attr(this.domTargetAttrs);
+            this.$domTarget = v.$(document.createElement(this.tagName ? this.tagName : 'div')).attr(this.domTargetAttrs);
             this.domTarget = this.$domTarget[0];
         }else{
             //Default to 'div'.
@@ -56,8 +50,7 @@ define('views', ['application', 'helpers'], function(){
     //0.5.0, 0.6.0
     function extend(viewObject, domEventsHash){
         //Create a new object using the view object as its prototype.
-        var obj1 =  v.helpers.extend(Object.create(proto), viewObject),
-            obj2 = Object.create(obj1);
+        var obj1 =  v.helpers.extend(Object.create(proto), viewObject), obj2 = Object.create(obj1);
         //0.6.0 Set domTarget && $domTarget
         setTarget.call(obj2);
         //0.6.0 Wire up events, if any are declared.
@@ -68,13 +61,7 @@ define('views', ['application', 'helpers'], function(){
         return obj2;
     }
 
-    proto = {
-        remove: function remove(){
-            this.$domTarget.off(this.namespace);
-            this.$domTarget.empty();
-        },
-        $: v.$
-    };
+    proto = {remove: function remove(){this.$domTarget.off(this.namespace); this.$domTarget.empty(); }, $: v.$};
 
     v.views = {extend: extend, domEventTopic: domEventTopic};
 });
